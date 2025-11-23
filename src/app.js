986b9f86018fb623cdc47dd157571014bd6948aa
{kanak} <{sharmakanak2108@gmail.com}>
2025-11-23 23:39:32 +0530

    started to learn node

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use("/user", (req, res)=>{
+    res.send("Hello from user")
+})
+
+app.use("/test", (req, res)=>{
+    res.send("Hello from tester....")
+})
+
+app.use("/hello", (req, res)=>{
+    res.send("Just shut up I'm tired of you!!!!")
+})
+
+app.listen(3000, ()=>{
+    console.log("Hello");
+});
