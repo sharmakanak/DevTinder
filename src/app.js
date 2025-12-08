9f47bb784df2ddaca9599b28701b45ebb8f70d00
{kanak} <{sharmakanak2108@gmail.com}>
2025-12-08 22:36:34 +0530

    Routes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,43 @@
 const express = require('express');
 const app = express();
 
-app.use("/user", (req, res)=>{
-    res.send("Hello from user")
-})
+// app.use("/user", (req, res)=>{
+//     res.send("Hello from user")
+// })
 
-app.use("/test", (req, res)=>{
-    res.send("Hello from tester....")
-})
+// app.use("/test", (req, res)=>{
+//     res.send("Hello from tester....")
+// })
 
-app.use("/hello", (req, res)=>{
+app.get("/user/:id/:name", (req, res)=>{
+    const param = {...req.params};
+    console.log(param);
+    res.send({
+    name :  'kanak',
+    age : '19',
+    city : 'haridwar'})
+})
+// app.get("/me", (req, res)=>{
+//     const query = {...req.query};
+//     console.log(query);
+//     res.send({
+//     name :  'kanak',
+//     age : '19',
+//     city : 'haridwar'})
+// })
+
+// app.post("/me", (req, res)=>{
+//     res.send("Data successfully saved");
+// })
+app.listen(3000, ()=>{
+    console.log("Hello xyz ");
+});
+
+app.get("/hello", (req, res)=>{
     res.send("Just shut up I'm tired of you!!!!")
 })
 
-app.listen(3000, ()=>{
-    console.log("Hello xyz");
-});
+app.delete("/hello", (req, res)=>{
+    res.send("Deleted successfully");
+})
+//s-2 e-4 t-29:06
