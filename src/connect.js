52e9084b31039d557c67f14a929b890594e19337
{kanak} <{sharmakanak2108@gmail.com}>
2025-12-12 20:46:06 +0530

    up

--- src/connect.js
+++ src/connect.js
@@ -24,7 +24,7 @@ app.post("/signup", async(req, res)=>{
     // });
     try{
         await user.save();
-        res.send("user added successfully!!!");
+        res.send("user added successfully");
     }
     catch(err){
         res.status(500).send("Error"+err.message);
