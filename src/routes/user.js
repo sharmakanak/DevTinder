c188bf1e924f3ea88586ae686e6552d68e888220
{kanak} <sharmakanak2108@gmail.com>
2026-01-29 21:18:03 +0530

    add user feed route to filter out users from connection requests

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,7 @@ const userRouter = express.Router();
 const {auth} = require("../middlewares/authUser");
 const ConnectionRequest = require("../models/connectionRequest");
 const { populate } = require("../models/user");
+const User = require("../models/user")
 userRouter.get("/user/request/received", auth, async(req, res)=>{
     try{
         const loggedInUser = req.user._id;
@@ -38,5 +39,41 @@ userRouter.get("/user/connection", auth, async(req, res)=>{
     catch(err){
         res.status(500).send("Something went wrong");
     }
+});
+
+userRouter.get("/user/feed", auth, async(req, res)=>{
+    try{
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+        const connectionRequests = await ConnectionRequest.find({
+            $or:[
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId")
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach((req)=>{
+           hideUsersFromFeed.add(req.fromUserId.toString());
+        hideUsersFromFeed.add(req.toUserId.toString());
+        });
+
+        const users = await User.find({
+            $and: [
+                {_id: {$nin: Array.from(hideUsersFromFeed)}},
+                {_id: {$ne: loggedInUser._id}},
+            ]
+        }).select("firstName LastName").skip(skip).limit(limit);;
+
+        res.json({data: users});
+    }
+    catch(err){
+        res.status(400).json({message: err.message});
+    }
+    
 });
 module.exports = userRouter;
